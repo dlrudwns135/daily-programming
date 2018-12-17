38dfc089ea11ba5107712240b6e43e11cfa22a4d
dlrudwns135 <dlrudwns135@hanmail.net>
2018-12-17 11:17:09 +0900

    add another answer

--- daily1/daily1.py
+++ daily1/daily1.py
@@ -3,8 +3,8 @@ def GetHex(dec):
 
 
 def Hexcolor(dec1, dec2, dec3):
-    #return '#' + GetHex(dec1) + GetHex(dec2) + GetHex(dec3)
-    return '#' + To_hex(dec1) + To_hex(dec2) + To_hex(dec3) #another answer
+    return '#' + GetHex(dec1) + GetHex(dec2) + GetHex(dec3)
+    #return '#' + To_hex(dec1) + To_hex(dec2) + To_hex(dec3) #another answer
 
 
 def To_hex(dec):
