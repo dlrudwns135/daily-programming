05b02f7f2a0fab3c37553c4728851972cb628c7a
dlrudwns135 <dlrudwns135@hanmail.net>
2018-12-24 22:52:23 +0900

    daily5

--- daily5/daily5.py
+++ daily5/daily5.py
@@ -1,8 +1,42 @@
+class Node:
+
+    def __init__(self, string):
+        self.data = string
+        self.children = []
+
+    def append(self, child):
+        self.children.append(child)
+
+
 data = [data.strip() for data in open('enable1_word_list', 'r')]
 
-def count_find_string(string):
-    strings = []
-    for i in range(0, string.__len__()):
-        substring = string[:i] + string[i+1:]
+max_depth = 0
+
+
+def make_tree(node):
+    for i in range(0, node.data.__len__()):
+        substring = node.data[:i] + node.data[i + 1:]
         if substring in data:
-            strings.append(substring)
+            child = Node(substring)
+            node.append(child)
+            make_tree(child)
+
+    # return node
+
+
+def count_max_length(node, count):
+    count += 1
+    if node.children.__len__() == 0:
+        global max_depth
+        if max_depth < count:
+            # global max_depth
+            max_depth = count
+    else:
+        for child in node.children:
+            count_max_length(child, count)
+
+
+root = Node('princesses')
+make_tree(root)
+count_max_length(root, 0)
+print(max_depth)
